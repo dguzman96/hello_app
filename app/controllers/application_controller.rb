dc353aa2a75e73f3311a00e9f1050b23d0526b74
Darren <darren.guzman15@gmail.com>
2021-02-15 14:38:25 -0500

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+
+  #def hello
+  #  render html: "¡HoLa, MuNdO!"
+  #end
+
+  =begin def goodbye
+    render html: "¡AdIoS mUnDo!"
+  end
+  =end
+end
